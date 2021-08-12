3905f03bdf4f1da4da0266a1d44f7d026f09051b
CocoCat56 <zo7758002@gmail.com>
2021-08-12 10:55:43 +0800

    加入 top 按鈕
    index.html 加入回到上方按鈕

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,39 @@
 $(window).mousemove(function(e){
     let x = e.pageX;
     let y = e.pageY;
-    $('#circle').offset({top: y, left: x});
+    $("#circle").offset({top: y, left: x});
 });
 $(window).scroll(function() {
+  // top
+  if($(this).scrollTop() > 800) {
+    $(".top_btn").removeClass("hidden_btn");
+  } else {
+    $(".top_btn").addClass("hidden_btn");
+  }
   // news
   //距離最新消息500px
-  if ( $( this ).scrollTop() > $( "#news" ).offset().top - 500 ) {
-    $( "#news .visible" ).each( function ( i ) {
+  if($(this).scrollTop() > $("#news").offset().top - 500) {
+    $("#news .visible").each(function (i) {
       //讓所有 visible delay 並刪除，加上 fadeInUp
-      $( this ).delay( i * 300 ).queue( function () {
-        $( this ).removeClass( "visible" ).addClass( "fadeInUp" );
-      } );
-    } );
+      $(this).delay(i * 300).queue(function() {
+        $(this).removeClass("visible").addClass("fadeInUp");
+      });
+    });
   }
   // recommend
-  if ( $( this ).scrollTop() > $( "#recommend" ).offset().top - 450 ) {
-    $( "#recommend .visible" ).queue( function () {
-        $( this ).removeClass( "visible" ).addClass( "fadeInUp" );
-      } );
+  if($(this).scrollTop() > $("#recommend").offset().top - 450) {
+    $("#recommend .visible").queue(function() {
+        $(this).removeClass("visible").addClass("fadeInUp");
+      });
   }
 });
 var mobile_view1100 = window.matchMedia("(max-width: 1100px)");
 if(mobile_view1100.matches) {
   $(function() {
-    $( "#news .visible" ).each( function ( i ) {
-      $( this ).delay( i * 300 ).queue( function () {
-        $( this ).removeClass( "visible" ).addClass( "fadeInUp" );
-      } );
+    $("#news .visible").each(function (i){
+      $(this).delay(i * 300).queue(function() {
+        $(this).removeClass("visible").addClass("fadeInUp");
+      });
     });
   });
 } 
